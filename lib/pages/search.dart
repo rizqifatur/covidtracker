815be4ad4a3d4e5df25bce20f0946ccbc169d982
Rizukii <rizqifatur4@gmail.com>
2020-04-30 05:47:29 +0700

    Adding Files

---
+++ lib/pages/search.dart
@@ -0,0 +1,122 @@
+import 'package:covid19_tracker/datasource.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+
+class Search extends SearchDelegate {
+  final List countryList;
+
+  Search(this.countryList);
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return ThemeData(
+      primaryColor: primaryBlack,
+      brightness: DynamicTheme.of(context).brightness
+    );
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final sugesstionList = query.isEmpty
+        ? countryList
+        : countryList
+            .where((element) =>
+                element['country'].toString().toLowerCase().startsWith(query))
+            .toList();
+
+    return ListView.builder(
+      
+      itemCount: sugesstionList.length,
+      itemBuilder: (context, index){
+               return Card(
+                  child: Container(
+                    height: 130,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 170,
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  sugesstionList[index]['country'],
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Image.network(
+                                  sugesstionList[index]['countryInfo']['flag'],
+                                  height: 50,
+                                  width: 60,
+                                ),
+                              ]),
+                        ),
+                        Expanded(
+                            child: Container(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  'CONFIRMED:' +
+                                      sugesstionList[index]['cases'].toString(),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.red),
+                                ),
+                                Text(
+                                  'ACTIVE:' +
+                                      sugesstionList[index]['active'].toString(),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blue),
+                                ),
+                                Text(
+                                  'RECOVERED:' +
+                                      sugesstionList[index]['recovered'].toString(),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.green),
+                                ),
+                                Text(
+                                  'DEATHS:' +
+                                      sugesstionList[index]['deaths'].toString(),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context).brightness==Brightness.dark?Colors.grey[100]:Colors.grey[900]),
+                                ),
+                              ]),
+                        ))
+                      ],
+                    ),
+                  ),
+                );
+    });
+  }
+}
